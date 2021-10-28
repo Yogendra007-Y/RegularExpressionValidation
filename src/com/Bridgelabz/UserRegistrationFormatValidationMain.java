a8f6bb5e8e300976cb09b320d4b03c3924b9d633
Yogendra <you@example.com>
2021-10-28 13:41:52 +0530

    Adding User Registration Format For First Name And Last Name Validation

--- src/com/Bridgelabz/UserRegistrationFormatValidationMain.java
+++ src/com/Bridgelabz/UserRegistrationFormatValidationMain.java
@@ -3,12 +3,29 @@ package com.Bridgelabz;
 import java.util.Scanner;
 
 public class UserRegistrationFormatValidationMain {
-    public static void main (String[] args){
-        System.out.println("\n****Welcome To User Registration Format Validation Program****");
-        UserRegistrationFormatValidation Validation = new UserRegistrationFormatValidation();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the name to validation");
-        String name = scanner.nextLine();
-        Validation.nameValidation(name);
+
+
+        public static void welcome() {
+            System.out.println("Welcome to User Registration System Problem");
+        }
+
+        public static void main(String[] args) {
+            welcome();
+            UserRegistrationFormatValidation user = new UserRegistrationFormatValidation();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter 1 : to validate First Name ");
+            System.out.println("Enter 2 : to validate Last Name ");
+            switch (scanner.nextInt()) {
+                case 1:
+                    UserRegistrationFormatValidation.validFirstName();
+                    break;
+                case 2:
+                    UserRegistrationFormatValidation.validLastName();
+                    break;
+                default:
+                    System.out.println("Select a valid number");
+            }
+        }
     }
-}
+
+
