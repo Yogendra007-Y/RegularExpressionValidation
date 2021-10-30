05284f0ef89a2d337b07e326202411a0273607fb
Yogendra <you@example.com>
2021-10-30 19:31:30 +0530

    Added Junit Test To Test Phone Number Format

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -25,4 +25,14 @@ public class UserRegistrationTest {
         boolean result = userRegistration.emailValidation("abc..@yahoo.com");
         Assert.assertFalse(result);
     }
+    @Test
+    public void giveNumberValue_WhenProper_ShouldReturnTrue(){
+        boolean result = userRegistration.numberValidation("10 1234567890");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenNumberValues_WhenNotProper_ShouldReturnFalse(){
+        boolean result = userRegistration.numberValidation("11 08450911888");
+        Assert.assertFalse(result);
+    }
 }
